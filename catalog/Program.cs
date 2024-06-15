dbde217c4696c137973c594ff34799206df11213
Manuel Martin <mjmm_manuel@hotmail.com>
2024-06-15 15:50:14 +0200

    upadate Program.cs (#21)

--- catalog/Program.cs
+++ catalog/Program.cs
@@ -1,4 +1,5 @@
 using GloboTicket.Catalog.Repositories;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,7 +13,23 @@ builder.Services.AddTransient<IEventRepository, AzureStorageEventRepository>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
+// Register the Swagger generator
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "GloboTicket API", Version = "v1" });
+});
+
 var app = builder.Build();
+
+// Enable middleware to serve generated Swagger as a JSON endpoint
+app.UseSwagger();
+
+// Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+// specifying the Swagger JSON endpoint
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "GloboTicket API V1");
+});
 
 app.UseAuthorization();
 
